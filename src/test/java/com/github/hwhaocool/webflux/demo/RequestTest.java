961c2139fd64816c077d340603bf4a7fc6ec002f
huangwei <huangwei@fanggeek.com>
2019-11-12 14:48:32 +0800

    test

--- src/test/java/com/github/hwhaocool/webflux/demo/RequestTest.java
+++ src/test/java/com/github/hwhaocool/webflux/demo/RequestTest.java
@@ -49,7 +49,7 @@ public class RequestTest {
     private MyRequest genRequest() {
         MyRequest myRequest = new MyRequest();
         myRequest.setMethod("get");
-        myRequest.setCount(200);
+        myRequest.setCount(1000);
         myRequest.setUrl("http://api-b-dev.kanfangjilu.com/v1/visitcard/estatecompanys");
         
         System.out.println("count = " + myRequest.getCount());
