ae4c37ac93beec880a02eff17a9f6f3989758eda
huangwei <huangwei@fanggeek.com>
2019-12-20 21:37:03 +0800

    加一个内存接口瞅瞅情况

--- src/main/java/com/github/hwhaocool/webflux/demo/controller/HealthCheckController.java
+++ src/main/java/com/github/hwhaocool/webflux/demo/controller/HealthCheckController.java
@@ -1,5 +1,10 @@
 package com.github.hwhaocool.webflux.demo.controller;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
+import java.lang.management.MemoryUsage;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.MediaType;
@@ -12,6 +17,8 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/stress")
 public class HealthCheckController {
+    
+    private static final String CHANGE_LINE = "\n";
     
     @GetMapping(value="/health", produces=MediaType.TEXT_PLAIN_VALUE)
     public Mono<String> health() {
@@ -29,6 +36,43 @@ public class HealthCheckController {
         }
         
         return Mono.just("ok");
+    }
+    
+    @GetMapping("/mem")
+    public Mono<String> mem() {
+        
+        StringBuilder sb = new StringBuilder(256);
+        
+        MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+        MemoryUsage nonHeapMemoryUsage = ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage();
+
+        List<MemoryPoolMXBean> memoryPoolMXBeans = ManagementFactory.getMemoryPoolMXBeans();
+        
+        sb.append("heap ->").append(heapMemoryUsage.toString()).append(CHANGE_LINE);
+
+        for (MemoryPoolMXBean poolMXBean : memoryPoolMXBeans) {
+            if (MemoryType.HEAP.equals(poolMXBean.getType())) {
+                MemoryUsage usage = poolMXBean.getUsage();
+                String poolName = beautifyName(poolMXBean.getName());
+                
+                sb.append(poolName).append("->").append(usage.toString()).append(CHANGE_LINE);
+            }
+        }
+
+        for (MemoryPoolMXBean poolMXBean : memoryPoolMXBeans) {
+            if (MemoryType.NON_HEAP.equals(poolMXBean.getType())) {
+                MemoryUsage usage = poolMXBean.getUsage();
+                String poolName = beautifyName(poolMXBean.getName());
+                
+                sb.append(poolName).append("->").append(usage.toString()).append(CHANGE_LINE);
+            }
+        }
+        
+        return Mono.just(sb.toString());
+    }
+    
+    private static String beautifyName(String name) {
+        return name.replace(' ', '_').toLowerCase();
     }
 
 }
